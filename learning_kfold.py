56ab3e21189f83fa222aa5cd59897d762d2627f3
DaeYoung7 <44760826+DaeYoung7@users.noreply.github.com>
2022-04-11 12:06:25 +0900

    ready to run

--- learning_kfold.py
+++ learning_kfold.py
@@ -1,3 +1,4 @@
+import inflect
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -16,15 +17,15 @@ def correlation(bm, data, date_idx, term):
 
 
 def data_loader(data, label, batch_size, seq_len):
-    train_x = []
-    train_y = []
+    x = []
+    y = []
     i = seq_len
     while i < len(data):
-        train_x.append(data[i-seq_len:i])
-        train_y.append(label.values[i])
-        i += 5
-    train_x_tensor = torch.tensor(np.array(train_x, dtype=np.float32))
-    train_y_tensor = torch.tensor(np.array(train_y, dtype=np.float32).reshape(-1,1))
+        x.append(data[i-seq_len:i])
+        y.append(label.values[i])
+        i += 1
+    train_x_tensor = torch.tensor(np.array(x, dtype=np.float32))
+    train_y_tensor = torch.tensor(np.array(y, dtype=np.float32).reshape(-1,1))
     train_dataset = TensorDataset(train_x_tensor, train_y_tensor)
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=False)
     return train_loader
@@ -35,43 +36,79 @@ batch_size = 32
 seq_len = 252
 corr_term = 252 * 1
 num_cnn = 64
+p = inflect.engine()
 
+def kfold(X, ret, label, num_fold, is_quantile):
+    taccus = []
+    vaccus = []
+    if is_quantile:
+        y = ret.copy()
+    else:
+        y = label.copy()
+    val_len = len(X) // num_fold
+    val_end_idx = len(X) - 1
+    val_end_date = X.index[val_end_idx]
+    val_start_idx = val_end_idx - val_len
+    val_start_date = X.index[val_start_idx]
+    train1_end_date = val_start_date - pd.DateOffset(months=1)
+    train1_end_idx = sum(X.index < train1_end_date)
+    train2_start_date, train2_start_idx = None, None
+    for i in range(num_fold):
+        train_x = pd.DataFrame()
+        train_y = pd.Series()
+        if train1_end_idx is not None:
+            train_x = pd.concat([train_x, X[:train1_end_idx]],axis=1)
+            train_y = pd.concat([train_y, y[:train1_end_idx]])
+        if train2_start_idx is not None:
+            train_x = pd.concat([train_x, X[train2_start_idx:]], axis=1)
+            train_y = pd.concat([train_y, y[train2_start_idx:]], axis=1)
+        val_x = X[val_start_idx:val_end_idx]
+        val_y = y[val_start_idx:val_end_idx]
 
-def train_test(X, ret, label, bm, is_quantile):
+        print(f'{p.ordinal(i+1)} fold learning')
+        taccu, vaccu = train_test(train_x, train_y, val_x, val_y, is_quantile)
+        taccus.append(taccu)
+        vaccus.append(vaccu)
+        print()
+        val_end_idx -= val_len
+        val_end_date = X.index[val_end_idx]
+        val_start_idx = val_end_idx - val_len
+        val_start_date = X.index[val_start_idx]
+        train2_start_date = val_start_date + pd.DateOffset(months=1) + pd.DateOffset(weeks=2)
+        train2_start_idx = X.index[train2_start_date]
+        if i == num_fold:
+            train1_end_idx = None
+            train1_end_date = None
+        else:
+            train1_end_date = val_start_date - pd.DateOffset(months=1)
+            train1_end_idx = X.index[train1_end_date]
+    print(f'Final Result  taccu {round(sum(taccus) / num_fold, 4)}  vaccu {round(sum(vaccus) / num_fold, 4)}')
+    return
+
+def train_test(train_x, train_y, val_x, val_y, is_quantile):
     tlosses = []
     vlosses = []
     if is_quantile:
-        y = ret.copy()
         loss_fn = quantile_loss
         criterion = 0.
     else:
-        y = label.copy()
         loss_fn = nn.BCELoss()
         criterion = 0.5
-    # 상관관계를 파악할 기간 (day)
-    train_data_end_date = pd.Timestamp(year=2016, month=1, day=1)
-    train_data_end_idx = sum(X.index < train_data_end_date)
-    val_start_date = train_data_end_date + pd.DateOffset(months=1)
-    val_start_idx = sum(X.index < val_start_date)
-    val_end_date = X.index[-1] - pd.DateOffset(months=1)
-    vala_end_idx = sum(X.index < val_end_date)
 
-    train_x = X[:train_data_end_idx]
     mms = MinMaxScaler((-1,1))
     train_x = mms.fit_transform(train_x)
-    train_y = y[:train_data_end_idx]
     train_loader = data_loader(train_x, train_y, batch_size, seq_len)
-    val_x = mms.transform(X[val_start_idx:vala_end_idx])
-    val_y = y[val_start_idx:vala_end_idx]
+    val_x = mms.transform(val_x)
     val_loader = data_loader(val_x, val_y, batch_size, seq_len)
 
-    net = MyModel(seq_len, len(X.columns), num_cnn, is_quantile)
+    net = MyModel(seq_len, train_x.shape[-1], num_cnn, is_quantile)
     optimizer = optim.Adam(net.parameters(), lr=0.001)
+    taccu, vaccu = 0., 0.
     for epoch in range(epochs):
         tloss = 0.0
         vloss = 0.0
-        tcorrect = 0.0
-        vcorrect = 0.0
+        taccu = 0.0
+        vaccu = 0.0
         total_len = 0.0
         net.train(True)
         for i, tdata in enumerate(train_loader, 1):
@@ -88,10 +125,10 @@ def train_test(X, ret, label, bm, is_quantile):
             if is_quantile:
                 label_copied[label_copied > criterion] = 1.0
                 label_copied[label_copied < criterion] = 0.0
-            tcorrect += torch.sum(outputs == label_copied).item()
+            taccu += torch.sum(outputs == label_copied).item()
             total_len += len(train_label)
         avg_loss = tloss / i
-        tcorrect /= total_len
+        taccu /= total_len
 
         total_len = 0.0
         net.train(False)
@@ -106,15 +143,15 @@ def train_test(X, ret, label, bm, is_quantile):
             if is_quantile:
                 label_copied[label_copied > criterion] = 1.0
                 label_copied[label_copied < criterion] = 0.0
-            vcorrect += torch.sum(outputs == label_copied).item()
+            vaccu += torch.sum(outputs == label_copied).item()
             total_len += len(val_label)
         val_avg_loss = vloss / i
-        vcorrect /= total_len
-        print(f'Epoch {epoch+1}  accuracy {round(tcorrect, 4)}  loss {round(avg_loss, 4)}  '\
-        f'val accuracy {round(vcorrect, 4)}  val loss {round(val_avg_loss, 4)}')
+        vaccu /= total_len
+        print(f'Epoch {epoch+1}  accuracy {round(taccu, 4)}  loss {round(avg_loss, 4)}  '\
+        f'val accuracy {round(vaccu, 4)}  val loss {round(val_avg_loss, 4)}')
         tlosses.append(round(avg_loss, 4))
         vlosses.append(round(val_avg_loss, 4))
     plt.plot(tlosses, label='train')
     plt.plot(vlosses, label='val')
     plt.show()
-    return
+    return taccu, vaccu
--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 from data_manager import *
-from learning_val import *
+from learning_kfold import *
 
 bm_name = 'K200'
 is_quantile = True
+num_fold = 3
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     data, data_all_day = read_data(bm_name)
     ret, label = make_label(data, data_all_day, bm_name)
     data, bm = preprocessing(data, data_all_day, bm_name)
     data = get_stationary(data)
+
+    # data에는 label을 알 수 없는 최근 데이터도 포함(1달 수익률이라 최근 1달 값은 알 수가 없음
+    data = data.loc[label.index]
     print('[Complete data managing]')
     print(f'data shape : {data.shape}')
     print('\n[Start Learning]')
-    train_test(data, ret, label, bm, is_quantile)
+    kfold(data, ret, label, num_fold, is_quantile)
     print('[Finish Learning]')
     # pred_label = save_result(y_pred, test_label, bm_name)
     # analyzing(pred_label, test_label)
