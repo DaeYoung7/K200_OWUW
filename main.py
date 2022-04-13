9614ee87c32d7c9e33e05b606601deee8ea6270f
DaeYoung7 <44760826+DaeYoung7@users.noreply.github.com>
2022-04-13 11:14:00 +0900

    add purged kfold

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from data_manager import *
 from learning import *
 
+is_test = False  # false to apply k-fold
 bm_name = 'K200'
 is_quantile = True
-ts_layer = 'transformer' # 'lstm'
+ts_layer = 'transformer'  # 'lstm'
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     data, data_all_day = read_data(bm_name)
@@ -15,7 +17,11 @@ if __name__ == '__main__':
     print('[Complete data managing]')
     print(f'data shape : {data.shape}')
     print('\n[Start Learning]')
-    y_pred, test_label = train_test(data, ret, label, bm, ts_layer, is_quantile)
+    if is_test:
+        y_pred, test_label = realtime_test(data, ret, label, bm, ts_layer, is_quantile)
+        pred_label = save_result(y_pred, test_label, bm_name)
+        analyzing(pred_label, test_label)
+    else:
+        num_fold = 5
+        purged_kfold(data, ret, label, num_fold, ts_layer, is_quantile)
     print('[Finish Learning]')
-    pred_label = save_result(y_pred, test_label, bm_name)
-    analyzing(pred_label, test_label)
