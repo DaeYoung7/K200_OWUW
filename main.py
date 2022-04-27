b5fd7eb94e472a6b89452f7c230ba9d4e87e815c
DaeYoung7 <44760826+DaeYoung7@users.noreply.github.com>
2022-04-27 13:30:47 +0900

    change input data to deep_factor

--- main.py
+++ main.py
@@ -8,25 +8,24 @@ bm_name = 'K200'
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--test', required=True)  # true - test, false - kfold
-    parser.add_argument('--quantile', required=True)  # true - quantile, false - binary classification
+    parser.add_argument('--test', default=False, action='store_true')  # true - test, false - kfold
+    parser.add_argument('--quantile', default=False, action='store_true')  # true - quantile, false - binary classification
     parser.add_argument('--ts_layer', required=True)  # transformer or lstm
     args = parser.parse_args()
 
     data, data_all_day = read_data(bm_name)
     ret, label = make_label(data, data_all_day, bm_name)
-    data, bm = preprocessing(data, data_all_day, bm_name)
-    data = get_stationary(data)
-    # data에는 최근 데이터까지 포함, 최근 한달 데이터는 label값이 없음(1달 수익률 구할 수 없기 때문)
-    data = data.loc[label.index]
+    bm = data[bm_name]
+    input_data = pd.read_csv('data/K200_deep_factor.csv', index_col='date', parse_dates=True)
     print('[Complete data managing]')
     print(f'data shape : {data.shape}')
     print('\n[Start Learning]')
-    if args.test:
-        y_pred, test_label = realtime_test(data, ret, label, bm, args.ts_layer, args.quantile)
+    if args.test is True:
+        print('asdf')
+        y_pred, test_label = realtime_test(input_data, ret, label, bm, args.ts_layer, args.quantile)
         pred_label = save_result(y_pred, test_label, bm_name, args.quantile)
         analyzing(pred_label, test_label)
     else:
         num_fold = 5
-        purged_kfold(data, ret, label, num_fold, args.ts_layer, args.quantile)
+        purged_kfold(input_data, ret, label, num_fold, args.ts_layer, args.quantile)
     print('[Finish Learning]')
