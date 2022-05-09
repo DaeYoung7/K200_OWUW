c55b2dd6bcfdd9af48d30fab8e6901cdab4d9264
DaeYoung7 <44760826+DaeYoung7@users.noreply.github.com>
2022-05-09 15:09:53 +0900

    add new ensemble version

--- learning.py
+++ learning.py
@@ -16,7 +16,76 @@ corr_term = 252 * 1
 p = inflect.engine()
 
 
-def realtime_test(X, y):
+def correlation(bm, data, date_idx, term):
+    corr = pd.concat([bm[date_idx-term:date_idx], data[date_idx-term:date_idx]], axis=1).corr()
+    corr_abs = abs(corr.iloc[0]).sort_values(ascending=False)
+    num_cols = len(corr_abs[corr_abs > 0.3].index[1:])
+    while num_cols % num_heads !=0:
+        num_cols += 1
+    # bm 제외
+    return corr_abs.index[1:num_cols+1]
+
+
+# version 1
+# 개별 모델(앙상블 이전) 학습 -> 학습 데이터에 대한 label생성
+# 동일한 학습데이터 + 생성된 label로 앙상블 모델 학습, 백테스트
+def direct_ensemble(X, y, bm, is_corr):
+    result = pd.DataFrame(columns=['LR', 'SVC', 'RF', 'label'])
+    month_check = 1
+    train_end_date = pd.Timestamp(year=2012, month=1, day=1)
+    test_date = train_end_date + pd.DateOffset(months=1)
+    while test_date < X.index[-1] - pd.DateOffset(weeks=1):
+        if month_check != train_end_date.month:
+            print(train_end_date)
+            month_check = train_end_date.month
+
+        train_end_idx = sum(X.index < train_end_date)
+        test_idx = sum(X.index < test_date)
+        if is_corr:
+            bm_related_cols = correlation(bm, X, train_end_idx, corr_term)
+        else:
+            bm_related_cols = X.columns
+        train_x = X[bm_related_cols][:train_end_idx].values
+        mms = MinMaxScaler((0, 1))
+        train_x = mms.fit_transform(train_x)
+        train_y = y[:train_end_idx].values.ravel()
+        test_x = mms.transform(X.iloc[test_idx][bm_related_cols].values.reshape((1,-1)))
+        test_y = y.iloc[test_idx].values[0]
+
+        # 개별 모델 학습, 학습 데이터에 대한 label 생성
+        LR_model = LogisticRegression(C=1e-5, penalty='l1', solver='saga')
+        LR_model.fit(train_x, train_y)
+        lr_train_pred = LR_model.predict(train_x)
+        lr_pred = LR_model.predict(test_x)[0]
+
+        SVC_model = SVC(C=30, gamma=0.001, probability=True)
+        SVC_model.fit(train_x, train_y)
+        svc_train_pred = SVC_model.predict(train_x)
+        svc_pred = SVC_model.predict(test_x)[0]
+
+        train_x_labeled = X[bm_related_cols][:train_end_idx].copy()
+        train_x_labeled['LR'] = lr_train_pred
+        train_x_labeled['SVC'] = svc_train_pred
+        train_x_labeled = train_x_labeled.values
+
+        test_x_labeled = np.append(test_x, np.array([[lr_pred, svc_pred]]), axis=1)
+
+        RF_model = RandomForestClassifier(max_depth=30, max_features=8, n_estimators=100)
+        RF_model.fit(train_x_labeled, train_y)
+        rf_pred = RF_model.predict(test_x_labeled)[0]
+
+        result.loc[X.index[test_idx]] = [lr_pred, svc_pred, rf_pred, test_y]
+
+        train_end_date += pd.DateOffset(weeks=1)
+        test_date = train_end_date + pd.DateOffset(months=1)
+    result.index.name = 'date'
+    return result
+
+# version 2
+# 데이터의 앞부분으로 개별 모델(앙상블 이전)모델 학습 -> 뒷부분에 대한 label 생성
+# 뒷부분으로 앙상블 모델 학습, 백테스트
+# 상관관계 적용 불가능 (개별 모델과 앙상블 모델의 학습 데이터 기간이 다르기 때문)
+def make_label_to_ensemble(X, y):
     result = pd.DataFrame(columns=list(X.columns)+['LR', 'SVC', 'label'])
 
     month_check = 1
--- main.py
+++ main.py
@@ -7,6 +7,10 @@ bm_name = 'K200'
 from sklearn.preprocessing import MinMaxScaler
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--version')
+    parser.add_argument('--corr', action='store_true', default=False)
+    args = parser.parse_args()
     # data, data_all_day = read_data(bm_name)
     # ret, label = make_label(data, data_all_day, bm_name)
     # data, bm = preprocessing(data, data_all_day, bm_name)
@@ -15,13 +19,19 @@ if __name__ == '__main__':
     # data = data.loc[label.index]
     data = pd.read_csv('data/data_preprocessed.csv', index_col='date', parse_dates=True)
     label = pd.read_csv('data/label.csv', index_col='date', parse_dates=True)
+    bm = pd.read_csv('data/bm_data.csv', index_col='date', parse_dates=True)
     print('[Complete data managing]')
     print(f'data shape : {data.shape}')
     print('\n[Start Learning]')
-    # result_data = realtime_test(data, label)
-    # result_data.to_csv('result/LR_SVC.csv')
-    result_data = pd.read_csv('result/LR_SVC.csv', index_col='date', parse_dates=True)
-    rf_pred, rf_label = ensemble(result_data)
+    if args.version == '1':
+        result_data = direct_ensemble(data, label, bm, args.corr)
+        print(result_data)
+        rf_pred = result_data['RF']
+        rf_label = result_data['label']
+    else:
+        result_data = make_label_to_ensemble(data, label)
+        result_data.to_csv('result/LR_SVC.csv')
+        rf_pred, rf_label = ensemble(result_data)
     analyzing(result_data['LR'], result_data['label'], 'LR')
     analyzing(result_data['SVC'], result_data['label'], 'SVC')
     analyzing(rf_pred, rf_label, 'RF')
